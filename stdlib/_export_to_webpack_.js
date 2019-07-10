37e0e162445efca2b66fbf83557cd2005d3bfb74
Maisa Milena <maisa.milena@gmail.com>
2019-07-10 12:57:18 +0000

    [Issue-9] Update Formality-Core's stdlib style

--- stdlib/_export_to_webpack_.js
+++ stdlib/_export_to_webpack_.js
@@ -1,36 +1,44 @@
-var array   = require("./array.fmc").default;
+var arr     = require("./arr.fmc").default;
 var bits    = require("./bits.fmc").default;
 var bool    = require("./bool.fmc").default;
-var hash    = require("./hash.fmc").default;
+var cat     = require("./cat.fmc").default;
+var global  = require("./global.fmc").default;
 var kaelin  = require("./kaelin.fmc").default;
+var keccak  = require("./keccak.fmc").default;
 var list    = require("./list.fmc").default;
-var main    = require("./main.fmc").default;
 var maybe   = require("./maybe.fmc").default;
+var main    = require("./main.fmc").default;
 var nat     = require("./nat.fmc").default;
 var num     = require("./num.fmc").default;
 var pair    = require("./pair.fmc").default;
+var pbt     = require("./pbt.fmc").default;
+var stack   = require("./stack.fmc").default;
 var string  = require("./string.fmc").default;
 var term    = require("./term.fmc").default;
 var tree    = require("./tree.fmc").default;
-var tuple   = require("./tuple.fmc").default;
-var vector2 = require("./vector2.fmc").default;
+var tup     = require("./tup.fmc").default;
+var v2      = require("./v2.fmc").default;
 
 module.exports = [
-  array,
+  arr,
   bits,
   bool,
-  hash,
+  cat,
+  global,
   kaelin,
+  keccak,
   list,
   main,
   maybe,
   nat,
   num,
   pair,
+  pbt,
+  stack,
   string,
   term,
   tree,
-  tuple,
-  vector2,
+  tup,
+  v2,
 ].join("\n");
 
